0f38c5b99a1771b713dc701b3c5848d016b27890
Erick Romero NIeto <64759936+Erickromero789@users.noreply.github.com>
2022-08-12 23:54:31 -0500

    Update CalculoDeNotas.cpp

--- CalculoDeNotas.cpp
+++ CalculoDeNotas.cpp
@@ -2,17 +2,20 @@
 using namespace std;
  
 //Función que nos da el Promedio
-float promedio(float nota1,float nota2, float nota3)
+float promedio(double nota1,double nota2, double nota3, double n)
 	{float Promedio;
-	Promedio = (nota1+nota2+nota3)/3;
+	Promedio = (nota1+nota2+nota3)/n;
 	return (Promedio);
 	}
  
 //Programa Principal
 int main()
 {
-  float nota1, nota2, nota3, i, n, notafinal;
-cout<<"Ingrese cantidad de alumnos:";
+  double nota1, nota2, nota3,notafinal,prom, i, n;
+  int cnt1=0, cnt2=0;
+  
+cout<<"\n*********** Calculo De Notas ***********\n";
+cout<<"\nIngrese cantidad de alumnos a registrar: ";
 cin>>n;
 //Bucle para ingreso de Notas, en base a 3 Unidades
 for (i=1;i<=n;i++){
@@ -29,19 +32,24 @@ for (i=1;i<=n;i++){
   nota2 = (nota2*0.3);
   
 	do{
-    cout<<"\nIngrese Nota Parcial: ";
+    cout<<"\nIngrese Nota Trabajos y Talleres: ";
 	  cin>>nota3;
   }while(nota3 < 0 || nota3 > 5.0);
   nota3 = (nota3*0.2);
 
-	notafinal = (nota1+nota2+nota3);
-  promedio(nota1,nota2,nota3);
-
-	//Condicion si el alumno supera el 10.5 (Nota mínima)
+	notafinal = nota1+nota2+nota3;
+  prom=notafinal/n;
+  //Condicion si el alumno supera el 10.5 (Nota mínima)
 	if (notafinal >= 3.0)
-		{cout<<"\nEl alumno ha aprobado la materia.\n";
+		{
+      cnt1 ++;
 		}
 	else
-		cout<<"\nEl alumno ha desaprobado la materia.\n";
-	}
+		cnt2 ++;
+}
+
+cout<<"\nAprobaron "<<cnt1<<" Estudiantes\n";
+cout<<"\nNo Aprobaron "<<cnt2<<" Estudiantes";
+cout<<"\nLa Nota Promedio es "<<prom;
+  
 }
